26a5b073fe9864f6b94f9c921c0bf485722175c0
Xin chen <czxinchen@Xins-MacBook-Air.local>
2023-01-05 10:46:08 -0700

    change 3001 to 3002 at server.js and apis restaurantFinder

--- server/server.js
+++ server/server.js
@@ -141,7 +141,7 @@ app.post("/api/v1/restaurants/:id/addReview", async (req, res) => {
   }
 });
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3002;
 app.listen(port, () => {
   console.log(`server is up and listening on port ${port}`);
 });
